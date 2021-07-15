0b5e0f20f353067e3529a2d305bb826fc1a0d9f0
Roberto Moran Tovar <r.morantovar@uni-koeln.de>
2021-07-15 14:14:54 +0200

    Organizing folders of the two different projects. Mac20

--- Lolli/Codes/Python/run_seir_ensemble.py
+++ Lolli/Codes/Python/run_seir_ensemble.py
@@ -15,7 +15,7 @@ from Epi_models import *
 
 print('Hola mundo \n')
 
-N_ensemble = 200
+N_ensemble = 400
 numNodes = 20
 baseGraph    = networkx.barabasi_albert_graph(n=numNodes, m=2)
 G_normal     = custom_exponential_graph(baseGraph, scale=100)
@@ -24,10 +24,18 @@ G_normal     = custom_exponential_graph(baseGraph, scale=100)
 # Quarantine interactions:
 #G_quarantine = custom_exponential_graph(baseGraph, scale=5)
 
+# ----- MODEL PARAMATERS -----
+
+R0 = 2.5
+gamma = 1/6
+lamda = 1/4
+sigma = 1e4
+beta = R0*gamma
+
 fig1, ax1 = plt.subplots(figsize = (10,8))
 fig2, ax2 = plt.subplots(figsize = (10,8))
 
-Q_days = np.array([0, 2, 4, 6, 8, 10, 12, 14, 20])
+Q_days = np.array([0, 4, 8, 12, 14, 18])
 
 colors_d = plt.cm.Blues(np.linspace(0,1,len(Q_days)+1))
 
@@ -37,9 +45,9 @@ for i, d in enumerate(Q_days):
 	I_cum = np.array([])
 	print('RUNNING ENSEMBLE with d = %d days... \n'%(d))
 	for n_ensemble in tqdm(np.arange(N_ensemble)):
-		model = ExtSEIRSNetworkModel(G=G_normal, beta=0.4, sigma=10000, lamda = 1/4, gamma = 1/6, gamma_asym=1/6, a = 1,  p=1,
+		model = ExtSEIRSNetworkModel(G=G_normal, beta=beta, sigma=sigma, lamda = lamda, gamma = gamma, gamma_asym=gamma, a = 1,  p=1,
 		                          G_Q=G_normal, initI_asym=1, isolation_time = d, sigma_Q = 1000, lamda_Q = 1/4, gamma_Q_asym = 1/6,
-		                          transition_mode = 'time_in_state', prevalence_ext = 1e-2, eta = 1, mu_H = 1, xi = 1e-10, nu = 1e-10)
+		                          transition_mode = 'time_in_state', prevalence_ext = 0, eta = 1, mu_H = 1, xi = 1e-10, nu = 1e-10)
 
 		run_tti_sim(model = model, T=7*4, testing_cadence = 'semiweekly2', testing_compliance_random = np.array([True]*model.numNodes), isolation_compliance_positive_individual = [True]*model.numNodes,
 			isolation_compliance_positive_groupmate = np.array([True]*model.numNodes), isolation_groups = np.array([range(numNodes)]*model.numNodes), 
@@ -55,11 +63,11 @@ for i, d in enumerate(Q_days):
 
 
 ax1.legend(title = 'Isolations days')
-my_plot_layout(ax=ax1)
+my_plot_layout(ax=ax1, xlabel = 'Infected')
 fig1.savefig('../../Figures/1_Extended_Model/histograms.pdf')
 
 ax2.plot(Q_days, I_avg_d)
-my_plot_layout(ax=ax2)
+my_plot_layout(ax=ax2, xlabel = 'Days', ylabel = 'Infected')
 fig2.savefig('../../Figures/1_Extended_Model/I_avg.pdf')
 
 
